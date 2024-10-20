5d3ee41849dd0bf9f92e1a4a40d1e93e90dcb9e0
Bogdan David <b.david@student.utwente.nl>
2024-10-20 19:39:49 +0200

    Added initial working version

--- application/source/dummy.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 2024 HV0rtex
-
-#include "httpclient.hpp"
-
-int main(int argc, char** argv) {
-    return 0;
-}
--- application/source/httpclient.cpp
+++ application/source/httpclient.cpp
@@ -2,10 +2,47 @@
 
 #include "httpclient.hpp"
 
+#include <string>
+#include <iostream>
+
 namespace downloader {
 
 HttpResponse HttpClient::GET(const std::string& host, const std::string& url) {
-    return HttpResponse();
+    HttpResponse response;
+
+    const auto address = resolver_.resolve(host, "80", ec_);
+
+    if (ec_.failed()) {
+        std::cout << "[ERR] Could not resolve host: " << host << "\n";
+        std::cout << "[ERR] Error code: " << ec_.message() << "\n";
+
+        return response;
+    }
+
+    stream_.connect(address, ec_);
+
+    if (ec_.failed()) {
+        std::cout << "[ERR] Could not establish connection to: " << host << "\n";
+        std::cout << "[ERR] Error code: " << ec_.message() << "\n";
+
+        return response;
+    }
+
+    HttpRequest request{http::verb::get, url, 12};
+    request.set(http::field::host, host);
+    request.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+    http::write(stream_, request);
+    http::read(stream_, buffer_, response);
+
+    stream_.socket().shutdown(tcp::socket::shutdown_both, ec_);
+
+    if (ec_.failed()) {
+        std::cout << "[WRN] Could not close connection to: " << host << "\n";
+        std::cout << "[WRN] Error code: " << ec_.message() << "\n";
+    }
+
+    return response;
 }
 
 }  // namespace downloader
